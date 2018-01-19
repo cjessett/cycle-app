acbabbb983af91dbabea888d9e0c46a17a2537f4
CJ Jessett <cjessett@gmail.com>
2018-01-18 23:20:52 -0600

    Update port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
